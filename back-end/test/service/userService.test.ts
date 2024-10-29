dd74607870b2191b29699a0d3706185eea06f3d8
deklercknathan <r0925964@ucll.be>
2024-10-29 17:32:43 +0100

    f

--- back-end/test/service/userService.test.ts
+++ back-end/test/service/userService.test.ts
@@ -1,2 +1,25 @@
+import { set } from 'date-fns';
 import { User } from '../../model/user';
+import userDb from '../../repository/user.db';
 import userService from '../../service/userService';
+
+let createUserMock: jest.Mock;
+
+let mockUserDbCreateUser: jest.SpyInstance<User, [User], any>;
+
+beforeEach(() => {
+    mockUserDbCreateUser = jest.spyOn(userDb, 'createUser');
+});
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+test('given a valid user, when schedule is user, then user is created with those values', () => {
+    userService.createUser({});
+
+    expect(mockUserDbCreateUser).toHaveBeenCalledTimes(1);
+    expect(mockUserDbCreateUser).toHaveBeenCalledWith(
+        new User({ name: 'f', email: 'e@a.c', password: 'lol', numPosts: 2 })
+    );
+});
