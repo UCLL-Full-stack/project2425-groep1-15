becb189f12c3573fd7b74a078b31b0e44c9f87d8
deklercknathan <r0925964@ucll.be>
2024-11-02 11:36:08 +0100

    fixed bs

--- back-end/app.ts
+++ back-end/app.ts
@@ -12,7 +12,7 @@ const app = express();
 dotenv.config();
 const port = process.env.APP_PORT || 3000;
 
-app.use(cors({ origin: 'http://localhost:8080' }));
+app.use(cors({ origin: 'http://localhost:8080/' }));
 app.use(bodyParser.json());
 
 app.use('/users', userRouter);
