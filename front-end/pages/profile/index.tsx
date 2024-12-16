48794391f8b8a4cdc3b6dddf748513839b915149
deklercknathan <r0925964@ucll.be>
2024-12-16 16:04:21 +0100

    checks in the sessionstorage for a user before showing the page

--- front-end/pages/profile/index.tsx
+++ front-end/pages/profile/index.tsx
@@ -1,13 +1,32 @@
 import Header from "@/components/header";
 import ProfileComponent from "@/components/users/Profile";
 import Head from "next/head";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const Profile: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const userData = sessionStorage.getItem("loggedInUser");
+
+    if (userData) {
+      try {
+        const parsedData = JSON.parse(userData);
+        setIsLoggedIn(!!parsedData.token);
+      } catch (error) {
+        console.error("Error parsing session storage data:", error);
+        setIsLoggedIn(false);
+      }
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
   return (
     <>
       <Head>
-        <title>profile</title>
+        <title>Profile</title>
         <meta name="description" content="Courses app" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
@@ -15,7 +34,16 @@ const Profile: React.FC = () => {
       <Header></Header>
       <main>
         <section>
-          <ProfileComponent />
+          {isLoggedIn === null ? (
+            <p>Loading...</p>
+          ) : isLoggedIn ? (
+            <ProfileComponent />
+          ) : (
+            <p>
+              Please log in <Link href="../login">here</Link> to view your
+              profile.
+            </p>
+          )}
         </section>
       </main>
     </>
